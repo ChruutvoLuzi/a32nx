32feec7dd3039313b1d6a7278df4f1268edec880
Will Pine <wpine215@gmail.com>
2021-07-18 13:36:13 -0700

    Re-organized file structure and added getGlidepathFPA()

--- src/fmgc/src/guidance/vnav/VnavDriver.ts
+++ src/fmgc/src/guidance/vnav/VnavDriver.ts
@@ -33,6 +33,11 @@ import { GuidanceComponent } from '../GuidanceComponent';
 import { Leg, TFLeg } from '../Geometry';
 import { GuidanceController } from '../GuidanceController';
 
+// Local imports
+import { FlapConf } from './common';
+import { EngineModel } from './EngineModel';
+import { FlightModel } from './FlightModel';
+
 export class VnavDriver implements GuidanceComponent {
     private guidanceController: GuidanceController;
 
@@ -107,7 +112,6 @@ export class VnavDriver implements GuidanceComponent {
         return this.flightplan.waypoints;
     }
 
-
     init(): void {
         console.log('[FMGC/Guidance] VnavDriver initialized!');
     }
@@ -126,6 +130,15 @@ export class VnavDriver implements GuidanceComponent {
         // - Manage constraints
         // - Calculate ToD
         //      - ToD present if we haven't descended to first
+    }
+
+    calculateTopOfClimb(): void {
+        // Calculated in TAKE-OFF or CLIMB phase
+        // Targets the lowest altitude of either a waypoint constraint, FCU altitude, or cruise level
+    }
+
+    calculateTopOfDescent(): void {
+
     }
 
     buildVerticalFlightPlan(): void {
@@ -214,6 +227,11 @@ export class VnavDriver implements GuidanceComponent {
         // pass
     }
 
+    /**
+     * Placeholder
+     * @param waypoint
+     * @returns
+     */
     parseConstraints(waypoint: WayPoint): Constraint {
         const constraints = {
             upperConstraint: Infinity,
@@ -256,6 +274,10 @@ export class VnavDriver implements GuidanceComponent {
         return constraints;
     }
 
+    /**
+     * Placeholder
+     * @returns
+     */
     getFirstApproachWaypointIndex(): number {
         const approach = this.fpm.getApproachWaypoints();
         if (approach && approach.length > 0) {
@@ -264,12 +286,70 @@ export class VnavDriver implements GuidanceComponent {
         return undefined;
     }
 
+    /**
+     * Placeholder
+     * @returns
+     */
     getLastApproachWaypointIndex(): number {
         const approach = this.fpm.getApproachWaypoints();
         if (approach && approach.length > 0) {
             return this.allWaypoints.indexOf(approach[approach.length - 1]);
         }
         return undefined;
+    }
+
+    /**
+     * Placeholder
+     * @returns
+     */
+    getLandingRunwayElevation(): number | undefined {
+        const approachRunway = this.fpm.getApproachRunway();
+        if (approachRunway !== undefined) {
+            return approachRunway.elevation;
+        }
+
+        // TODO: Get destination airport elevation as fallback
+        // const destAirport = this.fpm.getDestination();
+
+        return undefined;
+    }
+
+    /**
+     * Placeholder
+     * @returns
+     */
+    getGlidepathFPA(): number {
+        const approach = this.fpm.getApproachWaypoints();
+        if (approach.length > 0) {
+            const lastApproachWaypoint = approach[approach.length - 1];
+            if (lastApproachWaypoint.isRunway) {
+                let fafAltitude = undefined;
+                let fafDistance = undefined;
+                for (let i = approach.length - 2; i >= 0; i--) {
+                    const waypoint = approach[i];
+                    const distance = lastApproachWaypoint.cumulativeDistanceInFP - waypoint.cumulativeDistanceInFP;
+                    // Check if meets FAF conditions
+                    if (waypoint.legAltitudeDescription > 0 && waypoint.legAltitudeDescription < 3 && waypoint.legAltitude1 > 0 && distance > 3) {
+                        fafAltitude = waypoint.legAltitude1;
+                        fafDistance = lastApproachWaypoint.cumulativeDistanceInFP - waypoint.cumulativeDistanceInFP;
+                        break;
+                    }
+                }
+                const altitudeDifference = fafAltitude - lastApproachWaypoint.legAltitude1;
+                return VnavDriver.calculateFPA(altitudeDifference, fafDistance);
+            }
+        }
+        return undefined;
+    }
+
+    /**
+     * Placeholder
+     * @param verticalDistance
+     * @param lateralDistance
+     * @returns FPA
+     */
+    static calculateFPA(verticalDistance: number, lateralDistance: number): number {
+        return (180 / Math.PI) * (Math.atan(Math.abs(verticalDistance) / (lateralDistance * 6076.12)));
     }
 }
 
@@ -379,267 +459,11 @@ class VerticalSegment {
     }
 }
 
-class EngineModel {
-    /**
-     * Table 1502 - CN2 vs CN1 @ Mach 0, 0.2, 0.9
-     * n2_to_n1_table
-     * @param i row index (n2)
-     * @param j 1 = Mach 0, 2 = Mach 0.2, 3 = Mach 0.9 
-     * @returns Corrected N1 (CN1)
-     */
-    static table1502(i: number, j: number): number {
-        const t = [ 
-            [18.200000, 0.000000, 0.000000, 17.000000],
-            [22.000000, 1.900000, 1.900000, 17.400000],
-            [26.000000, 2.500000, 2.500000, 18.200000],
-            [57.000000, 12.800000, 12.800000, 27.000000],
-            [68.200000, 19.600000, 19.600000, 34.827774],
-            [77.000000, 26.000000, 26.000000, 40.839552],
-            [83.000000, 31.420240, 31.420240, 44.768766],
-            [89.000000, 40.972041, 40.972041, 50.092140],
-            [92.800000, 51.000000, 51.000000, 55.042000],
-            [97.000000, 65.000000, 65.000000, 65.000000],
-            [100.000000, 77.000000, 77.000000, 77.000000],
-            [104.000000, 85.000000, 85.000000, 85.500000],
-            [116.500000, 101.000000, 101.000000, 101.000000],
-        ];
-
-        return t[i][j];
-    }
-
-    /**
-     * Table 1503 - Turbine LoMach (0) CN2 vs. Throttle @ IAP Ratio 1.00000000, 1.20172257, 1.453783983, 2.175007333, 3.364755652, 4.47246108, 5.415178313
-     * mach_0_corrected_commanded_ne_table
-     * @param i row index (thrust lever position)
-     * @param j IAP ratio
-     * @returns Corrected N2 (CN2)
-     */
-    static table1503(i: number, j: number): number {
-        const t = [
-            [0.000000, 68.200000, 69.402657, 70.671269, 73.432244, 76.544349, 78.644882, 78.644882],
-            [0.100000, 76.000000, 77.340205, 78.753906, 81.830654, 85.298688, 87.639458, 87.639458],
-            [0.200000, 83.000000, 84.463645, 86.007556, 89.367688, 93.155146, 95.711513, 95.711513],
-            [0.400000, 92.800000, 94.436461, 96.162664, 99.919535, 104.154188, 107.012390, 107.012390],
-            [0.600000, 98.000000, 99.728159, 101.551090, 105.518475, 109.990414, 113.008774, 113.008774],
-            [0.750000, 101.500000, 103.289879, 105.177914, 109.286991, 113.918643, 117.044802, 117.044802],
-            [0.900000, 103.000000, 104.816330, 106.000000, 110.902070, 115.602170, 118.774528, 118.774528],
-            [1.000000, 104.200000, 106.037491, 107.975750, 112.194133, 116.948991, 120.158309, 120.158309],
-        ];
-
-        return t[i][j];
-    }
-
-    /**
-     * Table 1504 - Turbine HiMach (0.9) CN2 vs. Throttle @ IAP Ratio 1.00000000, 1.20172257, 1.453783983, 2.175007333, 3.364755652, 4.47246108, 5.415178313
-     * mach_hi_corrected_commanded_ne_table
-     * @param i row index (thrust lever position)
-     * @param j IAP ratio
-     * @returns Corrected N2 (CN2)
-     */
-    static table1504(i: number, j: number): number {
-        const t = [
-            [0.000000, 63.267593, 64.383271, 65.560133, 68.121427, 71.008456, 72.957073, 72.957073],
-            [0.100000, 70.503476, 71.746753, 73.058212, 75.912441, 79.129658, 81.301137, 81.301137],
-            [0.200000, 76.997217, 78.355007, 79.787258, 82.904376, 86.417916, 88.789399, 88.789399],
-            [0.400000, 86.088455, 87.606562, 89.207922, 92.693086, 96.621477, 99.272967, 99.272967],
-            [0.600000, 90.912377, 92.515550, 94.206642, 97.887095, 102.035612, 104.835676, 104.835676],
-            [0.750000, 94.159247, 95.819677, 97.571165, 101.383063, 105.679741, 108.579808, 108.579808],
-            [0.900000, 95.550763, 97.235732, 98.333795, 102.881334, 107.241510, 110.184435, 110.184435],
-            [1.000000, 104.200000, 106.037491, 107.975750, 112.194133, 116.948991, 120.158309, 120.158309],
-        ];
-
-        return t[i][j];
-    }
-
-    /**
-     * Table 1506 - Corrected net Thrust vs CN1 @ Mach 0 to 0.9 in 0.1 steps
-     * n1_and_mach_on_thrust_table
-     * @param i row index (CN1)
-     * @param j mach
-     * @returns Corrected net thrust (pounds of force)
-     */
-    static table1506(i: number, j: number) {
-        const t = [
-            [0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],
-            [20.000000, 0.091741, 0.057020, 0.031529, 0.014096, -0.017284, -0.037284, -0.057077, -0.205841, -0.315399, -0.488717],
-            [25.000000, 0.142810, 0.072215, 0.038026, 0.020404, -0.009593, -0.026571, -0.024556, -0.151328, -0.266204, -0.439028],
-            [30.000000, 0.189837, 0.082322, 0.04205, 0.026748, 0.017389, 0.003990, -0.026921, -0.056814, -0.081946, -0.369391],
-            [35.000000, 0.262207, 0.126047, 0.077206, 0.045921, 0.024719, 0.006062, -0.0028121, -0.022800, -0.06972, -0.293631],
-            [40.000000, 0.330230, 0.162757, 0.124088, 0.069579, 0.057905, 0.049621, 0.029790, 0.054284, 0.054218, -0.220630],
-            [45.000000, 0.393293, 0.250096, 0.156707, 0.112419, 0.091418, 0.076757, 0.056090, 0.018509, -0.057520, -0.155120],
-            [50.000000, 0.452337, 0.311066, 0.211353, 0.158174, 0.127429, 0.104915, 0.081171, 0.047419, -0.007399, -0.098474],
-            [55.000000, 0.509468, 0.373568, 0.269961, 0.209106, 0.168650, 0.137223, 0.108383, 0.075660, 0.028704, -0.049469],
-            [60.000000, 0.594614, 0.439955, 0.334629, 0.267477, 0.217773, 0.176899, 0.141404, 0.107148, 0.064556, -0.005036],
-            [65.000000, 0.660035, 0.512604, 0.407151, 0.335055, 0.276928, 0.226669, 0.183627, 0.145850, 0.104441, 0.039012],
-            [70.000000, 0.733601, 0.593506, 0.488571, 0.412623, 0.347163, 0.288210, 0.237559, 0.195142, 0.152485, 0.087269],
-            [75.000000, 0.818693, 0.683880, 0.578756, 0.499514, 0.427939, 0.361604, 0.304241, 0.257197, 0.212005, 0.144042],
-            [80.000000, 0.910344, 0.783795, 0.675982, 0.593166, 0.516644, 0.444822, 0.382689, 0.332384, 0.284867, 0.212679],
-            [85.000000, 1.025165, 0.891823, 0.776548, 0.688692, 0.608128, 0.533210, 0.469351, 0.418690, 0.370870, 0.294907],
-            [90.000000, 1.157049, 1.004695, 0.874400, 0.778466, 0.694251, 0.619011, 0.557581, 0.511153, 0.467149, 0.390203],
-            [95.000000, 1.281333, 1.116993, 0.960774, 0.851733, 0.763455, 0.690890, 0.637136, 0.601322, 0.567588, 0.495167],
-            [100.000000, 1.357935, 1.220844, 1.023864, 0.894234, 0.800352, 0.733488, 0.693684, 0.654691, 0.617963, 0.539115],
-            [105.000000, 1.378826, 1.239626, 1.048498, 0.915750, 0.819609, 0.751137, 0.710375, 0.670444, 0.632832, 0.552086],
-            [110.000000, 1.392754, 1.252148, 1.069322, 0.933937, 0.835886, 0.766054, 0.724483, 0.683759, 0.645400, 0.563051]
-        ];
-
-        return t[i][j];
-    }
-}
-
-class FlightModel {
-    static Cd0 = 0.0237;
-
-    static wingSpan = 117.5;
-
-    static wingArea = 1313.2;
-
-    static wingEffcyFactor = 0.75;
-
-    /**
-     * Get lift coefficient at given conditions
-     * @param weight in pounds
-     * @param mach self-explanatory
-     * @param delta pressure at the altitude divided by the pressure at sea level
-     * @param loadFactor g-Force
-     * @returns lift coefficient (Cl)
-     */
-    static getLiftCoefficient(weight: number, mach: number, delta: number, loadFactor = 1): number {
-        return (weight * loadFactor) / (1481.4 * (mach ** 2) * delta * this.wingArea);
-    }
-
-    /**
-     * Get drag coefficient at given conditions
-     * @param weight in pounds
-     * @param mach self-explanatory
-     * @param delta pressure at the altitude divided by the pressure at sea level
-     * @param spdBrkDeflected whether speedbrake is deflected at half or not
-     * @param gearExtended whether gear is extended or not
-     * @param flapConf flap configuration
-     * @returns drag coefficient (Cd)
-     */
-    static getDragCoefficient(weight: number, mach: number, delta: number, spdBrkDeflected = false, gearExtended = false, flapConf = FlapConf.CLEAN) : number {
-        const Cl = this.getLiftCoefficient(weight, mach, delta);
-
-        // Values taken at mach 0.78
-        let baseDrag;
-        switch (flapConf) {
-        case FlapConf.CLEAN:
-            baseDrag = (0.0384 * Cl ** 5) - (0.1385 * Cl ** 4) + (0.1953 * Cl ** 3) - (0.0532 * Cl ** 2) - (0.0052 * Cl) + 0.0259;
-            break;
-        case FlapConf.CONF_1:
-            baseDrag = (0.0438 * Cl ** 5) - (0.1911 * Cl ** 4) + (0.3215 * Cl ** 3) - (0.1801 * Cl ** 2) + (0.0281 * Cl) + 0.0441;
-            break;
-        case FlapConf.CONF_2:
-            baseDrag = (0.0116 * Cl ** 5) - (0.0593 * Cl ** 4) + (0.1292 * Cl ** 3) - (0.0858 * Cl ** 2) + (0.0043 * Cl) + 0.0895;
-            break;
-        case FlapConf.CONF_3:
-            baseDrag = (0.01 * Cl ** 5) - (0.0558 * Cl ** 4) + (0.1325 * Cl ** 3) - (0.1019 * Cl ** 2) + (0.0123 * Cl) + 0.1004;
-            break;
-        case FlapConf.CONF_FULL:
-            baseDrag = (0.0014 * Cl ** 5) - (0.0097 * Cl ** 4) + (0.0369 * Cl ** 3) - (0.0222 * Cl ** 2) - (0.0201 * Cl) + 0.1534;
-            break;
-        default:
-            break;
-        }
-
-        const spdBrkIncrement = spdBrkDeflected ? 0.01 : 0;
-        const gearIncrement = gearExtended ? 0.03 : 0;
-        return baseDrag + spdBrkIncrement + gearIncrement;
-    }
-
-    /**
-     * Get drag at given conditions
-     * @param weight in pounds
-     * @param mach self-explanatory
-     * @param delta pressure at the altitude divided by the pressure at sea level
-     * @param spdBrkDeflected Whether speedbrake is deflected at half or not
-     * @param gearExtended whether gear is extended or not
-     * @param flapConf flap configuration
-     * @returns drag
-     */
-    static getDrag(weight: number, mach: number, delta: number, spdBrkDeflected: boolean, gearExtended: boolean, flapConf: FlapConf): number {
-        const Cd = this.getDragCoefficient(weight, mach, delta, spdBrkDeflected, gearExtended, flapConf);
-        return 1481.4 * (mach ** 2) * delta * this.wingArea * Cd;
-    }
-
-    /**
-     * Gets acceleration factor for altitudes below troposphere
-     * @param mach self-explanatory
-     * @param temp actual temperature in Kelvin
-     * @param stdTemp standard day temperature in Kelvin
-     * @returns acceleration factor
-     */
-    static getAccelerationFactorBelowTropo(mach: number, temp: number, stdTemp: number): number {
-        return 1 - (0.133184 * mach ** 2) * (stdTemp / temp);
-    }
-
-    /**
-     * Gets acceleration factor for altitudes above troposphere
-     * @param mach self-explanatory
-     * @param flyingAtConstantMach if aircraft is flying at a constant mach
-     * @returns acceleration factor
-     */
-    static getAccelerationFactorAboveTropo(mach: number, flyingAtConstantMach: boolean): number {
-        if (flyingAtConstantMach) {
-            return 1;
-        }
-
-        const phi = (((1 + 0.2 * mach ** 2) ** 3.5) - 1) / ((0.7 * mach ** 2) * (1 + 0.2 * mach ** 2) ** 2.5);
-        return 1 + (0.7 * mach ** 2) * phi;
-    }
-
-    static getConstantThrustPathAngle(
-        thrust: number,
-        weight: number,
-        mach: number,
-        drag: number,
-        temp: number,
-        stdTemp: number,
-        altitude: number,
-        tropoAlt: number,
-        flyingAtConstantMach: boolean,
-    ): number {
-        const accelFactor = altitude >= tropoAlt ? this.getAccelerationFactorAboveTropo(mach, flyingAtConstantMach) : this.getAccelerationFactorBelowTropo(mach, temp, stdTemp);
-        return Math.asin(((thrust - drag) / weight) / accelFactor);
-    }
-
-    /**
-     * Gets distance required to accelerate/decelerate
-     * @param thrust 
-     * @param drag 
-     * @param weight in pounds
-     * @param initialSpeed 
-     * @param targetSpeed
-     * @returns distance to accel/decel
-     */
-    static getAccelerationDistance(
-        thrust: number,
-        drag: number,
-        weight: number,
-        initialSpeed: number,
-        targetSpeed: number,
-    ): number {
-        const force = thrust - drag;
-        const accel = force / weight; // TODO: Check units
-        const timeToAccel = (targetSpeed - initialSpeed) / accel;
-        const distanceToAccel = (initialSpeed * timeToAccel) + (0.5 * accel * (timeToAccel ** 2)); // TODO: Check units
-        return distanceToAccel;
-    }
-}
-
 enum VnavState {
     NONE,
     REPRESSURIZATION,
     IDLE,
     GEOMETRIC
-}
-
-enum FlapConf {
-    CLEAN,
-    CONF_1,
-    CONF_2,
-    CONF_3,
-    CONF_FULL
 }
 
 interface Constraint {
---
+++ src/fmgc/src/guidance/vnav/common.ts
@@ -0,0 +1,7 @@
+export enum FlapConf {
+    CLEAN,
+    CONF_1,
+    CONF_2,
+    CONF_3,
+    CONF_FULL
+}
